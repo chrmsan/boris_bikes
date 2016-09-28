56f1e1344a02f0f15df985e6e3b1a8807f3f0bb7
Chetan Sanghani <crsanghani@gmail.com>
2016-09-28 10:45:26 +0100

    completed task 12 still fixing test errors modified : docking_station.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -26,10 +26,9 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
+
 end
