4389cc72dba0515acbc2928de5a17e1993f96157
Chetan Sanghani <crsanghani@gmail.com>
2016-09-29 12:05:17 +0100

    wrote a failing test on bike_spec.rb

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe Bike do
   it { is_expected.to respond_to :working?}
+
+  it 'should be able to be reported broken' do
+    subject.is_broken
+    expect(subject).to be_broken 
+  end
 end
